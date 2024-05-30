4d6fdc9a129fd45adf7c5c9b5da737fd2392cf3a
Ritchmond James Tajarros <127801137+tajarrosr@users.noreply.github.com>
2024-05-30 20:54:40 +0800

    latest database
    added books table

--- booksforless.sql
+++ booksforless.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2024 at 05:34 PM
+-- Generation Time: May 30, 2024 at 02:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,6 +43,35 @@ CREATE TABLE `admins` (
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Ritchmond James Tajarros', 'tajarrosritchmond2002@gmail.com', '$2y$12$AKMR7QfhEkN/O2ZYIZIJ9e9vB5b31N4wp1l2oo9vNlU8sIv75CkBy', NULL, '2024-05-28 07:17:05', '2024-05-28 07:17:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `genre`, `description`, `price`, `quantity`, `picture`, `created_at`, `updated_at`) VALUES
+(2, 'Rich Dad Poor Dad', 'Robert T. Kiyosaki', 'Business & Economics, Finance', 'Rich Dad Poor Dad by Robert T. Kiyosaki is a personal finance classic that explores the different attitudes toward money and investing held by the author\'s two \"dads\": his biological father (Poor Dad) and the father of his best friend (Rich Dad). Through their contrasting philosophies, Kiyosaki presents key financial lessons on building wealth, financial independence, and the importance of financial education.', 750.00, 100, 'pictures/fXZggEzxdfoYPkkKqoYZH6kOmdrcRYjB5UOdeqGq.jpg', '2024-05-29 09:38:46', '2024-05-30 03:37:59'),
+(3, 'Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 'Fiction, Fantasy', 'Harry Potter and the Sorcerer\'s Stone is the first book in J.K. Rowling\'s acclaimed Harry Potter series. The story follows a young boy, Harry Potter, who discovers on his eleventh birthday that he is a wizard. Harry is whisked away to Hogwarts School of Witchcraft and Wizardry, where he uncovers secrets about his past, makes lifelong friends, and faces various magical adventures. This enchanting tale introduces readers to the magical world and sets the stage for Harry\'s epic journey.', 550.00, 150, 'pictures/MKWcHSLsZFhQPGQdeKAYeeC8JdL1elL0dW8VND9d.jpg', '2024-05-30 04:07:30', '2024-05-30 04:07:30'),
+(4, 'The Hunger Games', 'Suzanne Collins', 'Fiction, Young Adult', 'In a dystopian future, the nation of Panem is divided into 12 districts, each forced to send a boy and a girl, known as \"tributes,\" to compete in the annual Hunger Games, a televised fight to the death. Sixteen-year-old Katniss Everdeen volunteers to take her sister\'s place in the Games. Alongside Peeta Mellark, the male tribute from her district, Katniss must navigate the treacherous arena, outwit her opponents, and confront the Capitol\'s oppressive regime, all while grappling with complex emotions and the fight for survival.', 499.00, 50, 'pictures/1jYr8HmawifJ3XcDEkAlADDDolVUpFRlvApEjmpk.jpg', '2024-05-30 04:10:47', '2024-05-30 04:10:47'),
+(5, 'Frog and Toad Are Friends', 'Arnold Lobel', 'Children\'s Book, Early Reader', 'Frog and Toad Are Friends by Arnold Lobel is a beloved children\'s book that introduces young readers to the charming adventures of two best friends, Frog and Toad. Through a series of heartwarming and humorous stories, the book highlights themes of friendship, loyalty, and the joy of simple pleasures, making it an enduring classic for early readers.', 400.00, 100, 'pictures/EAGYHyijgo8bYfn0gAwjvr3DAUehEYdM64tHp6MH.jpg', '2024-05-30 04:15:28', '2024-05-30 04:15:28');
 
 -- --------------------------------------------------------
 
@@ -139,7 +168,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2024_05_28_150649_create_admins_table', 2);
+(4, '2024_05_28_150649_create_admins_table', 2),
+(5, '2024_05_29_165344_create_books_table', 3);
 
 -- --------------------------------------------------------
 
@@ -173,7 +203,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('uzQQAkzfP0yubWO5fuN3GnaeZQxE3r8PzjofDdl8', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibTc4SjJsY1dxTDBKZkhJS25HdFRsWGZzMHNJdEROTTY0MVdBTmplMCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTI6ImxvZ2luX2FkbWluXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1716909651);
+('x6zrcUWHqK48joBuKARCSDBYP9Q8T8x9HJueZLSS', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiSzBEU0V6WHBKa1Q2UDRxN3pPbzhsSUVmaGpaelNMeHZvVktrODA2UCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9pbnZlbnRvcnkiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUyOiJsb2dpbl9hZG1pbl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1717073185);
 
 -- --------------------------------------------------------
 
@@ -202,6 +232,12 @@ CREATE TABLE `users` (
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `admins_email_unique` (`email`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cache`
@@ -273,6 +309,12 @@ ALTER TABLE `admins`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -288,7 +330,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
